27387ab387344202f2925dc9853e9bab91f665fe
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2003-10-16 10:27:39 +0000

    Tagging the 0.10.0 release

---
+++ quad-pres/tags/versions/0.10.0/installer/tests/get-files-list.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use File::Copy;
+
+use XML::LibXML;
+
+my $files_or_dirs = shift || "-f";
+
+$files_or_dirs = ($files_or_dirs eq "-f");
+
+my $source_dir = shift || ".";
+
+my (@dirs, @files);
+sub process_dir
+{
+    my $dir = shift;
+
+    my $dir_slash = ($dir eq "") ? "" : "$dir/";
+
+    if ($dir ne "")
+    {
+        push @dirs, $dir;
+    }
+
+    my (@entries);
+    
+    {
+        my $parser = XML::LibXML->new();
+
+        my $dom = $parser->parse_file("$source_dir/${dir_slash}.svn/entries");
+
+        my @svn_entries = $dom->getDocumentElement()->getChildrenByTagName("entry");
+
+        foreach (@svn_entries)
+        {
+            my $deleted = $_->getAttribute("deleted");
+            if (defined($deleted) && ($deleted eq "true"))
+            {
+                next;
+            }
+            my $name = $_->getAttribute("name");
+            if ($name eq "svn:this_dir")
+            {
+                next;
+            }
+            push @entries, $name;
+        }
+    }
+
+    foreach my $entry (@entries)
+    {
+        my $rel_path = "${dir_slash}$entry";
+        my $full_path = "$source_dir/$rel_path";
+        if (-d $full_path)
+        {
+            process_dir($rel_path);
+        }
+        else
+        {
+            push @files, $rel_path;
+        }
+    }    
+}
+
+process_dir("");
+
+print map { "$_\n" } (($files_or_dirs) ? (@files) : (@dirs));
+
