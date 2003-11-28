1a652c9a0dc307657ff7498e8348afd10da25dc2
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2003-11-28 18:34:25 +0000

    Added a script to invalidate the records.

--- mini-reporter/trunk/upload.sh
+++ mini-reporter/trunk/upload.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+arg="$1"
+shift
+upload_to_base="shlomif@iglu.org.il:/iglu/cgi-bin/jobs/"
+upload_to="${upload_to_base}new/"
+if [ $arg == "--stable" ] ; then
+    upload_to="$upload_to_base"
+fi
 rsync -v --progress --rsh=ssh --relative \
     *.pl MyConfig.pm style.css Shlomif/MiniReporter.pm WWW/Form.pm \
-    shlomif@iglu.org.il:/iglu/cgi-bin/jobs/new/
+    "$upload_to"
