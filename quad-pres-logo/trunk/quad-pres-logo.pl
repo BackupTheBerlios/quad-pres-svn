b8f3b175ccf7e21ffe81395ec7029b29cf2cc462
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2004-01-20 17:42:25 +0000

    Added the beginning of the script to create the Quad-Pres Logo.

---
+++ quad-pres-logo/trunk/quad-pres-logo.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Gimp::Fu;
+
+# The letter height in pixels
+my $letter_height = 100 ;
+
+my $u_arc_height = int($letter_height/5);
+
+my $u_pipes_height = $letter_height - $u_arc_height;
+
+
+
