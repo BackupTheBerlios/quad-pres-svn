8b24da5f344cca74c36d753383273f4b3c59343e
shlomif <shlomif@6a489e09-e8c2-0310-a3fb-a128a961c0b2>
2004-01-21 20:01:13 +0000

    Modified gen-path.pl to have an OO interface for creating paths. Now maintaining offset integrity should be easier.

--- quad-pres-logo/trunk/gen-path.pl
+++ quad-pres-logo/trunk/gen-path.pl
@@ -1,5 +1,110 @@
 #!/usr/bin/perl -w
 
+package SVG::Path;
+
+use strict;
+
+sub new
+{
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    $self->initialize(@_);
+    return $self;
+}
+
+sub initialize
+{
+    my $self = shift;
+    my %args = (@_);
+
+    $self->{coords} = [($args{x} || 0), ($args{y} || 0)];
+    $self->flush_commands();
+    return $self;
+}
+
+sub _add_offset
+{
+    my $self = shift;
+    my (@off) = (@_);
+    for my $i ( 0 .. 1)
+    {
+        $self->{coords}->[$i] += $off[$i];
+    }
+}
+
+sub _add_cmd
+{
+    my $self = shift;
+    my $cmd = shift;
+
+    push @{$self->{cmds}}, $cmd;
+    return $cmd;
+}
+
+sub get_coords
+{
+    my $self = shift;
+
+    return [ @{$self->{coords}} ];
+}
+
+sub get_commands
+{
+    my $self = shift;
+
+    return [ @{$self->{cmds}} ];
+}
+
+sub flush_commands
+{
+    my $self = shift;
+    $self->{cmds} = [];
+}
+
+sub line_rel
+{
+    my $self = shift;
+    my (@off) = (@_); # The offset
+    my $ret = "l$off[0],$off[1]";
+    $self->_add_offset(@off);
+    return $self->_add_cmd($ret);
+}
+
+sub line_down
+{
+    my $self = shift;
+    my $len = shift;
+    return $self->line_rel(0, $len);
+}
+
+sub line_up
+{
+    my $self = shift;
+    my $len = shift;
+    return $self->line_down(-$len);
+}
+
+sub move_rel
+{
+    my $self = shift;
+    my (@off) = (@_); # The offset
+    my $ret = "m$off[0],$off[1]";
+    $self->_add_offset(@off);
+    return $self->_add_cmd($ret);
+}
+
+sub bezier_rel
+{
+    my $self = shift;
+    my (@off) = (@_); # The offsets
+    my $cmd = "c$off[0],$off[1] $off[2],$off[3] $off[4],$off[5]";
+    $self->_add_offset(@off[4..5]);
+    return $self->_add_cmd($cmd);
+}
+
+package main;
+
 use strict;
 
 use SVG;
@@ -14,43 +119,26 @@ my $u_pipes_height = $letter_height - $u_arc_height;
 
 my $u_width = int($letter_height * 1);
 
-my @offset = (0,0);
-
-sub line_rel
-{
-    my ($x,$y) = @_;
-    my $ret = "l$x,$y ";
-    $offset[0] += $x;
-    $offset[1] += $y;
-    return $ret;
-}
-
-sub line_down
-{
-    my $len = shift;
-    return line_rel(0,$len);
-}
-
-sub line_up
-{
-    my $len = shift;
-    return line_down(-$len);
-}
+my $path = SVG::Path->new();
 
 sub gen_u
 {
     my $curve_len = $u_arc_height * 2;
-    return line_down($u_pipes_height) ."c0,$curve_len $u_width,$curve_len $u_width,0 " . line_up($u_pipes_height);
+    $path->line_down($u_pipes_height);
+    $path->bezier_rel(0,$curve_len,$u_width,$curve_len,$u_width,0);
+    $path->line_up($u_pipes_height);
 }
 
 sub gen_path
 {
-    return "m10,10 . " . gen_u();
+    $path->move_rel(10,10);
+    gen_u();
+    return join("", map { "$_ " } @{$path->get_commands()});
 }
 
 my $svg = SVG->new(width => 1024, height => 256);
 
-my $path = 
+my $svg_path = 
     $svg->path(
         id => "quadpres",
         style => 
